a48397f020a27165121686a58eb536c77f3acac4
Scalikoglu <scalikoglu@yandex.com>
2023-02-12 20:37:19 +0300

    -Kazanma Oranı hesaplama eklendi.

--- src/app/components/item-xp-calculator/item-xp-calculator.component.ts
+++ src/app/components/item-xp-calculator/item-xp-calculator.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemXpCalculatorComponent implements OnInit {
 
+  currentTotalGame:number = 0  //Oynanan toplam maç sayısı
+  currentWinRate:number = 0  //Anlık kazanma oranı
+  currentAimRate:number = 0  //Hedeflenen kazanma oranı
+  hiddenDiv: boolean = false  //Gizli divi açıyoruz
+
+  currentTotalLose:number = 0  //Hesaplanan toplam kaybedilen maç sayısı
+  needTotalGame:number = 0  //Hesaplanan toplam oynanan maç sayısı ve ardarda kazanılması gereken maç sayısının toplamı
+  needWinGame:number = 0  //Ardarda kazanılması gereken maç sayısı
+  winRate:number = 0  //Hesaplama sonucu currentWinRate'i yüzdelik şekilde yazıyoruz
+  aimRate:number = 0  //Hesaplama sonucu currentAimRate'i yüzdelik şekilde yazıyoruz
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  setTotalGame(value: any){
+    this.currentTotalGame = value.target.value
+    this.calculateIt()
+  }
+  setRateGame(value: any){
+    this.currentWinRate = value.target.value
+    this.calculateIt()
+  }
+  setAimRateGame(value: any){
+    this.currentAimRate = value.target.value
+    this.calculateIt()
+  }
 
+  calculateIt(){
+
+  this.winRate = 1-(this.currentWinRate*0.01)
+  this.currentTotalLose = this.currentTotalGame * (1-(this.currentWinRate*0.01))
+  this.aimRate = 1-(this.currentAimRate*0.01)
+  this.needTotalGame = this.currentTotalLose / this.aimRate
+  this.needWinGame = this.needTotalGame - this.currentTotalGame
+
+  }
+
+  setHiddenDiv(){
+    if(this.hiddenDiv==true)
+    this.hiddenDiv = false
+    else
+    this.hiddenDiv = true
+  }
 }
